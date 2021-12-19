fc17f88c50aab4514f01c62769fe515235af5b07
Tejaswini <tejaswininimbalkar2000@gmail.com>
2021-12-19 18:08:11 +0530

    reverse a string using stack

---
+++ Basic DSA/Stacks/reverse_string_using_stack.cpp
@@ -0,0 +1,25 @@
+//Reverse a string using stack
+//TC: O(n) AS: O(n)
+//GFG Problem: https://practice.geeksforgeeks.org/problems/reverse-a-string-using-stack/1#
+
+#include<iostream>
+#include<stack>
+#include<string>
+using namespace std;
+
+//return the address of the string
+char* reverse(char *S, int len)
+{
+    //code here
+    char* arr;
+    arr = new char[len];
+    stack<int> st;
+    for(int i = 0; i < len; i++){
+        st.push(S[i]);
+    }
+    for(int i = 0; i < len; i++){
+        arr[i] = st.top();
+        st.pop();
+    }
+    return arr;
+} 
