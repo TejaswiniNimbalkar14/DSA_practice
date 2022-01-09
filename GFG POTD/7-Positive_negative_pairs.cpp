801b2bbdcde5d239c600e4e0c2e1707447eba1e2
Tejaswini <tejaswininimbalkar2000@gmail.com>
2022-01-09 14:45:56 +0530

    solved positive negative pairs

---
+++ GFG POTD/7-Positive_negative_pairs.cpp
@@ -0,0 +1,23 @@
+//Positive negative pairs
+//GFG: https://practice.geeksforgeeks.org/problems/positive-negative-pair5209/1#
+
+class Solution{
+  public:
+    //Function to return list containing all the pairs having both
+    //negative and positive values of a number in the array.
+    vector <int> findPairs(int arr[], int n) 
+    {
+        // code here
+        vector<int> res;
+        map<int, int> mp;
+        for(int i = 0; i < n; i++){
+            int temp = abs(arr[i]);
+            mp[temp]++;
+            if(mp[temp] == 2){
+                res.push_back(-temp);
+                res.push_back(temp);
+            }
+        }
+        return res;
+    }
+};
